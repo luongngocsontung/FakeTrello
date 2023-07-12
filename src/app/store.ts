b1dad5f2226789e5e26a3158880bce0adbefa4eb
Lee <luongngocsontung@gmail.com>
2023-07-12 18:59:24 +0700

    Remove counter leftover

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,12 +1,10 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
-import counterReducer from "../features/counter/counterSlice";
 import trelloReducer from "../features/FakeTrello/trelloSlice";
 import listsReducer from "../features/Lists/listsSlice";
 import taskReducer from "../features/Tasks/taskSlice";
 
 export const store = configureStore({
     reducer: {
-        counter: counterReducer,
         trello: trelloReducer,
         lists: listsReducer,
         tasks: taskReducer,
